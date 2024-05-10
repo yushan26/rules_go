dd35e7d2d5bc465ac5f6337f20979abbd96e9ea3
DolceTriade <hmodi@aviatrix.com>
2024-05-10 04:11:09 -0700

    generate_test_main: Move timeout handling back to bzltestutil (#3939)
    This gives us a determinstic location to ignore for goleak, which we
    previously had, but was removed as part of #3920.

--- go/tools/builders/generate_test_main.go
+++ go/tools/builders/generate_test_main.go
@@ -98,13 +98,11 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"os/signal"
 {{if .TestMain}}
 	"reflect"
 {{end}}
 	"strconv"
 	"strings"
-	"syscall"
 	"testing"
 	"testing/internal/testdeps"
 
@@ -241,27 +239,7 @@ func main() {
 	testTimeout := os.Getenv("TEST_TIMEOUT")
 	if testTimeout != "" {
 		flag.Lookup("test.timeout").Value.Set(testTimeout+"s")
-		// If Bazel sends a SIGTERM because the test timed out, it sends it to all child processes. Because
-		// we set -test.timeout according to the TEST_TIMEOUT, we need to ignore the signal so the test has
-		// time to properly produce the output (e.g. stack trace). It will be killed by Bazel after the grace
-		// period (15s) expires.
-		//
-		// If TEST_TIMEOUT is not set (e.g., when the test binary is run by Delve for debugging), we don't
-		// ignore SIGTERM so it can be properly terminated. (1)
-		// We do not panic (like native go test does) because users may legitimately want to use SIGTERM
-		// in tests.
-		//
-		// signal.Notify is used to ensure that there is a no-op signal handler registered.
-		// Avoid using signal.Ignore here: despite the name, it's only used to unregister handlers that
-		// were previously registered by signal.Notify. See (2) for more information.
-		//
-		// (1): https://github.com/golang/go/blob/e816eb50140841c524fd07ecb4eaa078954eb47c/src/testing/testing.go#L2351
-		// (2): https://github.com/bazelbuild/rules_go/pull/3929
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGTERM)
-		go func() {
-			<-c
-		}()
+		bzltestutil.RegisterTimeoutHandler()
 	}
 
 	{{if not .TestMain}}
